99e18033ea2ec41fdcbfa861e98fb58dccecab72
MiaoMint <miaomint@0u0.ren>
2024-08-03 01:05:38 +0800

    Badge

---
+++ components/Model.tsx
@@ -0,0 +1,67 @@
+"use client";
+import * as THREE from "three";
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { GLTF } from "three-stdlib";
+
+type GLTFResult = GLTF & {
+  nodes: {
+    Object_4: THREE.Mesh;
+    Object_5: THREE.Mesh;
+    Object_7: THREE.Mesh;
+  };
+  materials: {
+    main: THREE.MeshStandardMaterial;
+    hrom: THREE.MeshStandardMaterial;
+  };
+};
+
+interface ModelProps extends React.ComponentProps<"group"> {
+  image?: File;
+}
+
+export function Model(props: ModelProps) {
+  const { nodes, materials } = useGLTF("/scene.gltf") as GLTFResult;
+  const getMaterial = (image: File) => {
+    const texture = new THREE.TextureLoader().load(URL.createObjectURL(image));
+    texture.colorSpace = THREE.SRGBColorSpace;
+    const material = new THREE.MeshStandardMaterial({
+      map: texture,
+    });
+    return material;
+  };
+
+  const material = props.image ? getMaterial(props.image) : materials.main;
+
+  return (
+    <group {...props} dispose={null}>
+      <group position={[0, 0.001, 0]} rotation={[Math.PI / 2, 0, 0]}>
+        <group position={[0, 0.298, 0]} scale={[1, 0.079, 1]}>
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Object_4.geometry}
+            material={material}
+            rotation={[-Math.PI, 0, -Math.PI]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Object_5.geometry}
+            material={materials.hrom}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Object_7.geometry}
+            material={materials.hrom}
+            position={[-0.121, -4.461, 0]}
+            scale={[0.285, 2.486, 0.221]}
+          />
+        </group>
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload("/scene.gltf");
